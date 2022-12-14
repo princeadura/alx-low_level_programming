88f0efd452c56b2b6e69c50dbd88caea36bb9adb
princeadura <princeadura5@gmail.com>
2022-12-15 05:28:57 +0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  
+// Function to print the alphabet
+// in lower case
+void lowercaseAlphabets()
+{
+    // for lowercase
+    for (char c = 'a'; c <= 'z'; ++c)
+        printf("%c ", c);
+    printf("\n");
+}
